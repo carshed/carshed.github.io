56de2de0e0fc98a495c5956840bf3a350fb3c03a
carshed <48885936+assistmen@users.noreply.github.com>
2019-04-20 08:45:12 +0300

    Zalivka02

---
+++ index2.php
@@ -0,0 +1,368 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>CARSHED - Выкупаем неисправные и разбитые  автомобили и избавляем вас от</title>
+    <!-- Meta Tegs -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <!-- Styles --> 
+    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/remodal.css">
+    <link rel="stylesheet" href="css/remodal-default-theme.css">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet">
+    
+    <!-- Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <!-- Jquery Byblia -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/remodal.min.js"></script> 
+</head>
+<body>
+
+	<div id="header">
+		<div id="header-center">
+			<div id="header-block">
+
+				<div id="header-left">
+					<p>
+						<a href="index.php">CARSHED</a>
+						<span>
+							Выкупаем неисправные и разбитые<br>
+							автомобили и избавляем вас от
+						</span>
+					</p>
+				</div>
+                
+                <div id="responsive-menu">
+                    <a href="tel:+79819650055">
+                       <div id="responsive-phone">
+                            <i class="fas fa-phone"></i>
+                       </div>
+                    </a>   
+                       <div id="responsive-bar" onclick="open_menu();">
+                            <i class="fas fa-bars"></i>
+                       </div>
+                </div>
+
+				<div id="header-right">
+					<div id="header-menu">
+						<div id="header-menu-li">
+							<a href="http://carshed.ru/start/" style="padding-left: 0px; padding-right: 32px; border: none;">
+							   <b style="padding-left: 0px;">О нас</b>
+						    </a>
+						</div>
+						<div id="header-menu-li">
+							<a href="tel:+79819650055">
+								<img src="images/phone.png"> <b>+7 (981) 965-00-55</b>
+							</a>
+						</div>
+					</div>
+				</div>
+
+			</div>	
+		</div>	
+	</div>	
+	<div id="menu-resp">
+       <ul>
+         <li><a href="#home">О Нас</a></li>
+         <li><a href="tel:+79819650055">+7 (981) 965-00-55</a></li>
+         <li style="width: -webkit-fill-available; height: auto; display: inline-block; padding-bottom: 12px; border-bottom: none;">
+         	<a style="font-size: 14px;">
+         	  Выкупаем неисправные и разбитые<br>
+         	  автомобили и избавляем вас от
+         	 </a>
+         </li>
+       </ul>
+    </div>
+
+	<div id="slider">
+		<div id="slider-center">
+			<div id="slider-block">
+				<h1>
+					Пройдите тест и получите на 10%<br>
+                    больше денег за Ваш автомобиль
+				</h1>
+				<span>
+					Cписок из 5 вопросов поможет нам учесть наиболее важные аспекты вашей<br>
+                    ситуации и сэкономит время для расчёта автомобиля
+				</span>
+
+				<div id="slider-button-block">
+				  <center>
+				  	 <a href="http://carshed.ru/">
+				  	   <button>Пройти за 1 мин</button>
+				  	 </a>  
+				  </center>
+				</div>  
+
+				<div id="slider-top">
+					<div id="slider-top-div">
+						<a href="#gallery"><img src="images/top.png"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="gallery">
+		<div id="gallery-center">
+
+			<h2>Выкуп битых авто</h2>
+            <hr class="gallery-element">
+			<span>Более чем за 3 года работы мы выкупили в сумме не одну площадку</span>
+
+			<div id="gallery-block">
+
+				<div id="gallery-img" class="galleryN1" style="background-image: url(images/gallery.png);">
+					<div id="gallery-img-opacity">
+						<center>
+						   <img src="images/zoom.png">
+						</center>   
+					</div>
+				</div>	
+				<div id="gallery-img" class="galleryN2" style="background-image: url(images/gallery.png);">
+					<div id="gallery-img-opacity">
+						<center>
+						   <img src="images/zoom.png">
+						</center>   
+					</div>
+				</div>
+				<div id="gallery-img" class="galleryN3" style="background-image: url(images/gallery.png);">
+					<div id="gallery-img-opacity">
+						<center>
+						   <img src="images/zoom.png">
+						</center>   
+					</div>
+				</div>
+				<div id="gallery-img" class="galleryN4" style="background-image: url(images/gallery.png);">
+					<div id="gallery-img-opacity">
+						<center>
+						   <img src="images/zoom.png">
+						</center>   
+					</div>
+				</div>
+				<div id="gallery-img" class="galleryN5" style="background-image: url(images/gallery.png);">
+					<div id="gallery-img-opacity">
+						<center>
+						   <img src="images/zoom.png">
+						</center>   
+					</div>
+				</div>
+				<div id="gallery-img" class="galleryN6" style="background-image: url(images/gallery.png);">
+					<div id="gallery-img-opacity">
+						<center>
+						   <img src="images/zoom.png">
+						</center>   
+					</div>
+				</div>
+                
+                <!-- ALL GALLERY -->
+				<!--<a href="#" class="all-gallery">показать еще <b>+</b></a>-->
+
+			</div>
+		</div>
+	</div>
+
+	<div id="sale">
+		<div id="sale-center">
+			<div id="sale-block">
+				<div id="sale-left">
+					<p>
+						<b>+10%</b><br>
+						<span>к стоимости продажи</span>
+					</p>
+				</div>
+				<div id="sale-right">
+					<p>
+						Уделите буквально 1 минуту времени и получите наиболее выгодную
+                        на рынке цену за Ваш аварийный или неисправный автомобиль
+                        <br><br><br>
+                        <a href="http://carshed.ru/">
+                           <button>Пройти за 1 мин</button>
+                        </a>   
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="comment">
+		<div id="comment-center">
+			<div id="comment-block">
+				
+				<div id="comment-block-ctr">
+					<p class="comment-ph">
+					  <b class="comment-ph-b">Отзывы о нашей работе</b>
+				      <hr class="comment-element">
+
+				      <span class="comment-pages">
+				      	<a class="comment-left" onclick="pageOne();">
+				      		<img src="images/left.png">
+				      	</a>
+
+				      	   <b style="color: #ff9900;" id="commentPageOne">1</b> 
+				      	   / 
+				      	   <b id="commentPageTwo">2</b>
+
+				      	<a class="comment-right" onclick="pageTwo();">
+				      		<img src="images/right.png">
+				      	</a>
+				      </span>
+				    </p>
+                
+                    <div id="comment-display" class="comment-display-page1">	
+                       
+                            <div id="com">
+                	   <div id="info-com">
+                	   	  <p>
+                	   	  	<img src="images/comment1.png" class="comment-img"> 
+                	   	  	<b>Максим</b><br>	
+                	   	  	<span>Москва</span>	
+                	   	  </p>
+                	   </div>
+                	   <span class="comment-text">
+                	   	  “Попав в неприятное ДТП сначала не знал что делать с разбитой
+                	   	  машиной, потом перерыв интернет и знакомых узнал, что продать
+                	   	  как есть будет проще чем ремонтировать и терять кучу времени.
+                	   	  Посоветовали продать в компанию Carshed и получить за свою
+                	   	  машину больше на 10%, что я и сделал. Спасибо за оперативность
+                	   	  и спецпредложение!”
+                	   </span>
+                            </div>
+                            <div id="com">
+                	   <div id="info-com">
+                	   	  <p>
+                	   	  	<img src="images/comment2.png" class="comment-img"> 
+                	   	  	<b>Мария</b><br>	
+                	   	  	<span>Тверь</span>	
+                	   	  </p>
+                	   </div>
+                	   <span class="comment-text">
+                	   	  “ “Как так случилось, сама не поняла, но вдруг попала в аварию. Сразу 
+                	   	  паника, страх, безысходность. Потом набрала номер что нашла в
+                	   	  интернете и получила быстрый ответ от сотрудника который и успокоил
+                	   	  и дал возможность быстро сориентироваться в ситуации. Я
+                	   	  обрадовалась, когда услышала что все заботы ребята возьмут на себя и, 
+                	   	  тому же, деньги за мою разбитую Мазду я получу в тот же день.”
+                	   </span>
+                            </div>
+            
+                    </div>	
+
+                     <div id="comment-display" class="comment-display-page2" style="display: none;">	
+                       
+                            <div id="com">
+                	   <div id="info-com">
+                	   	  <p>
+                	   	  	<img src="images/comment1.png" class="comment-img"> 
+                	   	  	<b>Максим</b><br>	
+                	   	  	<span>Москва</span>	
+                	   	  </p>
+                	   </div>
+                	   <span class="comment-text">
+                	   	  Попав в неприятное ДТП сначала не знал что делать с разбитой машиной, потом перерыв интернет и знакомых узнал, что продать как есть будет проще чем ремонтировать и терять кучу времени. Посоветовали продать в компанию Carshed и получить за свою машину больше на 10%, что я и сделал. Спасибо за оперативность и спецпредложение!”
+                	   </span>
+                            </div>
+                            <div id="com">
+                	   <div id="info-com">
+                	   	  <p>
+                	   	  	<img src="images/comment1.png" class="comment-img"> 
+                	   	  	<b>Максим</b><br>	
+                	   	  	<span>Москва</span>	
+                	   	  </p>
+                	   </div>
+                	   <span class="comment-text">
+                	   	  Попав в неприятное ДТП сначала не знал что делать с разбитой машиной, потом перерыв интернет и знакомых узнал, что продать как есть будет проще чем ремонтировать и терять кучу времени. Посоветовали продать в компанию Carshed и получить за свою машину больше на 10%, что я и сделал. Спасибо за оперативность и спецпредложение!”
+                	   </span>
+                            </div>
+            
+                    </div>	
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="info">
+		<div id="info-center">
+			<div id="info-block">
+				<div id="info-left">
+					<p>
+						<b>Выкуп битых авто</b>
+						<hr class="info-element">
+						<span>
+							Подготовку вашего транспортного средства к продаже хорошо решать<br>
+							с рассмотрения полученных сколов. Предпродажу затратного седана к<br>
+							стоку принято начинать с расценки случившихся царапин.<br>
+							Предпродажную подготовку упавшего автомобиля к передаче лучше<br>
+							заканчивать с оценки многочисленных ударов. Организацию плохого<br>
+							тс к выкупу важно уметь делать с скупка битых машин выявления 
+						</span>
+					</p>
+				</div>
+				<div id="info-right">
+					<img src="images/info.png">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="sale-calculator">
+		<div id="sale-calculator-center">
+			<div id="sale-calculator-block">
+				<div id="sale-calculator-left">
+					<span>Хочу рассчитать стоимость с учётом надбавки</span>
+				</div>
+				<div id="sale-calculator-right">
+					<a href="http://carshed.ru/" style="text-decoration: none;">
+						<button>рассчитать стоимость</button>
+				    </a>		
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="footer">
+		<div id="footer-center">
+			<div id="footer-block">
+				<div id="footer-left">
+					<ul>
+						<li style="margin-left: -40px; font-family: 'Museo Sans 900 Black';"><a href="index.php">CARSHED</a></li>
+						<li><a href="http://carshed.ru/start/vikup/">Оценка автомобиля</a></li>
+						<li><a href="http://carshed.ru/onetimeoffer/">Главная</a></li>
+						<li><a href="http://carshed.ru/start/">О нас</a></li>
+					</ul>
+					<p>© Copyright 2016 Carshed - все права защищены</p>
+				</div>
+				<div id="footer-right">
+					<a href="#header"><img src="images/to-bottom.png"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<a id="view_gallery" data-remodal-target="gallery"></a>
+
+  <!-- Order Modal -->
+    <div class="remodal remodal-is-initialized remodal-is-opened" style="max-width: 500px; background: none;" data-remodal-id="gallery" tabindex="-1">
+      <button data-remodal-action="close" class="remodal-close"></button>
+         <p style="font-size: 12px; width: 100%; margin: 0 auto;">
+           <img src="" id="vv_img" style="width: 100%;">
+         </p>
+    </div>
+
+
+<script src="js/scroll.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
